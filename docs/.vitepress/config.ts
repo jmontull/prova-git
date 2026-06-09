9bf26c2664bdb4af99bd83622e5f2736385af4f9
jmontull <jaume82@gmail.com>
2026-06-09 20:05:51 +0200

    Update base path in VitePress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
     title: 'Apunts SMX',
-    base: '/prova_git/',
+    base: '/prova-git/',
 })
