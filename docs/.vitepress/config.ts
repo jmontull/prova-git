e8824d40a0011f9f460345a71c07bea2d7869af8
jmontull <jaume82@gmail.com>
2026-06-09 19:49:13 +0200

    segon commit

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+    title: 'Apunts SMX',
+    base: '/prova_git/',
+})
